7a37df81b92f0f2e7614c5e2f699bec7473a537e
Anas Lamaiz <lamaizanass@gmail.com>
2020-11-22 06:06:22 +0100

    ba9i masalit hhh

--- app/src/main/java/com/zeneo/omechle/MainActivity.java
+++ app/src/main/java/com/zeneo/omechle/MainActivity.java
@@ -1,6 +1,11 @@
 package com.zeneo.omechle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
@@ -10,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        AppBarConfiguration appBarConfiguration =
+                new AppBarConfiguration.Builder(navController.getGraph()).build();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        NavigationUI.setupWithNavController(toolbar, navController, appBarConfiguration);
     }
 }
